a1f8f99510fcc91c071f14b609b5bf55d18dc24a
ssmalik <ssmalik989@gmail.com>
2025-01-04 23:55:56 +0530

    day 3

---
+++ 30_DaysOfJs/Day2/exercise1.js
@@ -0,0 +1,27 @@
+const variable = "30 Days Of JavaScript";
+
+console.log(variable);
+
+console.log(variable.length);
+
+console.log(variable.toUpperCase());
+
+console.log(variable.toLowerCase());
+
+console.log(variable.substring(0, 1));
+
+console.log(variable.substring(3));
+
+console.log(variable.includes("Script"));
+
+console.log(variable.split());
+
+console.log(variable.split(" "));
+
+console.log(
+  "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(", ")
+);
+
+console.log(variable.replace("JavaScript", "Python"));
+
+console.log(variable.charAt(15));
