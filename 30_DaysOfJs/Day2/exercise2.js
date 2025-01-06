f27cb27f9be7094a2b617e1d0158718cc1678d8c
ssmalik <ssmalik989@gmail.com>
2025-01-06 23:55:25 +0530

    day 4 exercise 2

---
+++ 30_DaysOfJs/Day2/exercise2.js
@@ -0,0 +1,27 @@
+console.log(
+  "There is no exercise better for the heart than reaching down and lifting people up."
+);
+
+console.log(
+  "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+);
+
+console.log(typeof +"10" == typeof 10);
+
+let stringNumber = "9.8";
+
+let convertedNum = parseFloat(stringNumber);
+
+console.log(Math.ceil(convertedNum));
+
+const toCheck = "on";
+
+console.log("python".includes(toCheck));
+
+console.log("jargon".includes(toCheck));
+
+const sentence = "I hope this course is not full of jargon";
+
+const word = "jargon";
+
+console.log(sentence.includes(word), "check");
