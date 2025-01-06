9403c59dc4af58bc1fb3a58619def053a92d93cb
ssmalik <ssmalik989@gmail.com>
2025-01-06 20:46:15 +0530

    day 4

--- 30_DaysOfJs/Day2/exercise1.js
+++ 30_DaysOfJs/Day2/exercise1.js
@@ -25,3 +25,42 @@ console.log(
 console.log(variable.replace("JavaScript", "Python"));
 
 console.log(variable.charAt(15));
+
+console.log(variable.charAt(11));
+
+console.log(variable.charCodeAt(11));
+
+console.log(variable.indexOf("a"));
+
+console.log(variable.lastIndexOf("a"));
+
+console.log(
+  "You cannot end a sentence with because because because is a conjunction".indexOf(
+    "because"
+  )
+);
+
+console.log(
+  "You cannot end a sentence with because because because is a conjunction".lastIndexOf(
+    "because"
+  )
+);
+
+console.log("hii");
+console.log(
+  "You cannot end a sentence with because because because is a conjunction".search(
+    "because"
+  )
+);
+
+console.log("   30 Days Of JavaScript   ".trim());
+
+console.log(variable.startsWith("30"));
+
+console.log(variable.endsWith("ipt"));
+
+console.log(variable.match(/a/gi));
+
+console.log("30 Days Of".concat(" JavaScript"));
+
+console.log(variable.repeat(2));
